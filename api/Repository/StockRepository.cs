8ecc15df2959f045102360069f5c115f7089c45a
ericbr0wne <ericbrowne@hotmail.com>
2024-09-24 20:37:27 +0200

    GetAll([FromQuery] QueryObject query)

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -42,7 +42,6 @@ namespace api.Repository
 
         public async Task<List<Stock>> GetAllAsync(QueryObject query)
         {
-
             var stocks = _context.Stocks.Include(c => c.Comments).AsQueryable();
             if(!string.IsNullOrWhiteSpace(query.CompanyName))
             {
